f615018218eae408f4b9557ff37d09644d7a5677
Leo Ho <leo.ho@mindera.com>
2024-06-06 18:51:32 +0100

    button inserted in example

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,14 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
+import Head from 'next/head';
+import Image from 'next/image';
+import { Inter } from 'next/font/google';
+import styles from '@/styles/Home.module.css';
+import Link from 'next/link';
 
 const inter = Inter({ subsets: ['latin'] })
+
+function Button() {
+  return (<button>Click me!</button>);
+}
 
 export default function Home() {
   return (
@@ -15,7 +20,9 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={`${styles.main} ${inter.className}`}>
+        <Button></Button>
         <div className={styles.description}>
+          <h1 className='title'>Read <Link href='/posts/first-post'>this page!</Link></h1>
           <p>
             Get started by editing&nbsp;
             <code className={styles.code}>src/pages/index.js</code>
